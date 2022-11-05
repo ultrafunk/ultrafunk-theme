0f0b5d7d54e9b64b58e283464665a15dcad90e9e
ultrafunk.com <rune@ultrafunk.com>
2022-11-05 14:10:58 +0100

    1.44.66

--- php/constants.php
+++ php/constants.php
@@ -11,7 +11,7 @@ namespace Ultrafunk\Theme\Constants;
 /**************************************************************************************************************************/
 
 
-const VERSION = '1.44.65';
+const VERSION = '1.44.66';
 
 const THEME_ENV = [
   'menu_item_all_tracks_id' => WP_DEBUG ? 4057 : 4430,
