56fa4d421a9bd6f1ac2040609f82453308136966
ultrafunk.com <rune@ultrafunk.com>
2026-01-18 06:05:43 +0100

    1.48.42

--- php/templates/content-list-player.php
+++ php/templates/content-list-player.php
@@ -8,6 +8,11 @@
 namespace Ultrafunk\Theme\Templates;
 
 
+/**************************************************************************************************************************/
+if (!defined('ABSPATH')) exit;
+/**************************************************************************************************************************/
+
+
 use Ultrafunk\Plugin\Shared\Constants\TRACK_TYPE;
 
 use function Ultrafunk\Plugin\Shared\Utils\get_term_links;
